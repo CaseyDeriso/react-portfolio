51c93aed78f3e0f8beb63f0597f870618dd18b05
Casey Deriso <caseyderiso@gmail.com>
2021-03-03 17:11:49 -0600

    created dynamic class name to style active link on Nav using react short circuit evaluation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav";
-import "./index.css"
+import "./index.css";
 
-const app = () => (
-  <>
-    <Nav></Nav>
-  </>
-);
+const App = () => {
+  const sections = useState([
+    {
+      name: "About Me",
+      description: "I'm a dude, he's a dude, she's a dude, wer're all dudes!",
+    },
+    {
+      name: "Portfolio",
+      description: "I've done some cool things.",
+    },
+    {
+      name: "Contact",
+      description: "Contact me!",
+    },
+    {
+      name: "Resume",
+    },
+  ]);
 
-export default app;
+  const [sectionSelected, setSectionSelected] = useState(sections[0]);
+  return (
+    <>
+      <Nav
+        sections={sections}
+        setSectionSelected={setSectionSelected}
+        sectionSelected={sectionSelected}
+      ></Nav>
+    </>
+  );
+};
+
+export default App;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
 const NavBar = styled.header`
@@ -8,34 +8,71 @@ const NavBar = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 0 10px
+  margin: 0 10px;
 `;
 
 const HeaderText = styled.h1`
-  color: tomato
-`
+  color: tomato;
+`;
 
 const NavElement = styled.a`
-  color: grey;
   cursor: pointer;
-`
+`;
 const ButtonWrapper = styled.div`
   display: flex;
   min-width: 75%;
   max-width: 50vw;
   justify-content: space-around;
-`
+`;
 
 function Nav(props) {
+  const { sections = [], sectionSelected, setSectionSelected } = props;
+
+  useEffect(() => {
+    document.title = "test";
+  }, [sectionSelected]);
+
   return (
     <NavBar>
-    <HeaderText>Casey Deriso</HeaderText>
-    <ButtonWrapper>
-      <NavElement onClick={()=> console.log("hi")}>Contact</NavElement>
-      <NavElement>Resume</NavElement>
-      <NavElement>Portfolio</NavElement>
-      <NavElement>About Me</NavElement>
-    </ButtonWrapper>
+      <HeaderText>Casey Deriso</HeaderText>
+      <ButtonWrapper>
+        {sections[0].map((selection) => {
+          return (
+            <NavElement
+              active={false}
+              key={selection.name}
+              onClick={(e) => setSectionSelected(e.target.textContent)}
+              className={ sectionSelected === selection.name ? "active" : ""}
+            >
+              {selection.name}
+            </NavElement>
+          );
+        })}
+        {/* 
+        <NavElement
+          onClick={(e) => setSectionSelected(e.target.textContent)}
+        >
+          Contact
+        </NavElement>
+        <NavElement
+          className={sectionSelected === sections[0][1] && 'active'}
+          onClick={(e) => setSectionSelected(e.target.textContent)}
+        >
+          Resume
+        </NavElement>
+        <NavElement
+          className={sectionSelected === sections[0][2] && 'active'}
+          onClick={(e) => setSectionSelected(e.target.textContent)}
+        >
+          Portfolio
+        </NavElement>
+        <NavElement
+          className={sectionSelected === sections[0][3] && 'active'}
+          onClick={(e) => setSectionSelected(e.target.textContent)}
+        >
+          About Me
+        </NavElement> */}
+      </ButtonWrapper>
     </NavBar>
   );
 }
