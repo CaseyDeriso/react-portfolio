9ac543025322f47d9ae29c781f0d2618519d0da9
Casey Deriso <caseyderiso@gmail.com>
2021-03-14 11:35:46 -0500

    added background images to projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,33 +3,54 @@ import styled from "styled-components";
 
 const PortfolioDiv = styled.div`
   display: grid;
-  grid-gap: 5px;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  margin: 0 10px;
-`
+`;
 
 const Project = styled.div`
-  background-color: #0081a7;
+  font-family: arial;
+  background-size: cover;
   padding: 8px;
+`;
+
+const ProjectHeader = styled.h4`
+  align-items: center;
+  background-color: #fed9b7;
+  opacity: 80%;
+  display: inline-block;
+  padding: 0 5px;
 `
 
+const ProjectText = styled.p`
+  background-color: #fed9b7;
+  opacity: 80%;
+  display: inline-block;
+  padding: 0 5px;
+`
+
+
 function Portfolio(props) {
-  const { projects } = props
-  console.log(projects)
+  const { projects } = props;
   return (
     <PortfolioDiv>
       {projects.map((project, i) => {
-        const  {name, description} = project;
+        const { name, description, screenshot, link } = project;
+        const background = require(`../../assets/photos/${screenshot}`).default
         return (
-        <Project key={i} >
-          <h4>{name}</h4>
-          <p>{description}</p>
-        </Project>
-        )
+          <Project
+            href={link}
+            key={i}
+            style={{
+              backgroundImage: `url(${background})`
+            }}
+          >
+            <ProjectHeader>{name}</ProjectHeader>
+            <ProjectText>{description}</ProjectText>
+          </Project>
+        );
       })}
     </PortfolioDiv>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
