8c0c8e8aeccdab2c511fff9febc764efc948b20e
Casey Deriso <caseyderiso@gmail.com>
2021-04-16 13:50:05 -0500

    added links to contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,11 +4,13 @@ import styled from "styled-components";
 // Forms, inputs, buttons
 
 const FormWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 90vh;
+  overflow: none;
+`;
 
 const Form = styled.form`
   width: 300px;
@@ -29,7 +31,7 @@ const TextArea = styled.textarea`
   height: 200px;
   border: 1px soild #ccc;
   background-color: #fff;
-`
+`;
 
 const Button = styled.button`
   width: 300px;
@@ -39,32 +41,61 @@ const Button = styled.button`
   border-radius: 3px;
 `;
 
-// Text
-
 const Title = styled.h1`
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   font-weight: 600;
   color: #4d4d4d;
   font-size: 2.2em;
 `;
 
+const ContactGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
 
-
+const Icon = styled.a`
+  font-size: 4em;
+  color: rgb(0, 0, 0);
+  padding: 0.2em;
+  text-decoration: none;
+`;
 
 function Contact() {
-
-
-
-  return(
+  return (
     <FormWrapper>
-    <Title>Contact Me</Title>
-    <Form>
-      <Input type="text" name="email" placeholder="Email" />
-      <TextArea type="text" name="messgae" placeholder="Message" />
-      <Button type="submit">Submit</Button>
-    </Form>
-  </FormWrapper>
-  )
+      <Title>Contact Me</Title>
+      <ContactGrid>
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+        <Icon
+          href={"https://github.com/CaseyDeriso"}
+          target="_blank"
+          className="ri-github-fill"
+        />
+        <Icon
+          href={"https://www.linkedin.com/in/casey-deriso-b789409b/"}
+          target="_blank"
+          className="ri-linkedin-box-fill"
+        />
+        <Icon
+          href={"mailto:caseyderiso@gmail.com"}
+          target="_blank"
+          className="ri-mail-add-fill"
+        />
+      </ContactGrid>
+      <Form onSubmit={(e) => {
+        e.preventDefault();
+        e.target.reset();
+      }}>
+        <Title>Email me directly</Title>
+        <Input type="email" name="email" placeholder="Your Email" />
+        <TextArea type="text" name="messgae" placeholder="Message" />
+        <Button type="submit">Submit</Button>
+      </Form>
+    </FormWrapper>
+  );
 }
 
 export default Contact;
