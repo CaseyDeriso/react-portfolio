6e978ea700fa73d24a8212e6e1ef81b8b0394fe3
Casey Deriso <caseyderiso@gmail.com>
2021-04-16 13:18:02 -0500

    sredesigned modal to scale better on large screen and styled close button

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 const ModalBackdrop = styled.div`
@@ -9,34 +9,55 @@ const ModalBackdrop = styled.div`
   right: 0;
   top: 0;
   z-index: 1;
-  background-color: rgba(125,125,125, 0.8)
-`
+  background-color: rgba(125, 125, 125, 0.8);
+`;
 
 const ImageContainer = styled.div`
   display: flex;
+  flex: 1;
   justify-content: center;
   align-items: center;
-  padding: 5em 2em;
-`
+  padding: 5em 15em;
+  @media only screen and (max-width: 1000px) {
+    padding: 5em 2em;
+  }
+  @media only screen and (max-width: 700px) {
+    padding: 5em 2em;
+  }
+`;
 
 const Image = styled.img`
-  width: 100%
-`
+  width: 100%;
+`;
 
 const Close = styled.button`
+  border-radius: 50%;
+  height: 2.5em;
+  width: 2.5em;
+  color: rgba(0, 0, 0, 0.8);
+  background-color: rgba(175, 175, 175, 0.4);
   position: absolute;
-  right: 1em;
-  top: 1em;
-`
+  top: 6.5em;
+  right: 19em;
+  @media only screen and (max-width: 1000px) {
+    width: 1.8em;
+    height: 1.8em;
+    right: 3em;
+    top: 6.5em;
+  }
+`;
 
-export default function Modal({ onClose, currentPhoto}) {
+export default function Modal({ onClose, currentPhoto }) {
   return (
     <ModalBackdrop>
       <ImageContainer>
         {/* <h3>{name}</h3> */}
-        <Image src={require(`../../assets/photos/${currentPhoto}`).default} alt={currentPhoto}/>
+        <Image
+          src={require(`../../assets/photos/${currentPhoto}`).default}
+          alt={currentPhoto}
+        />
         <Close onClick={onClose}>X</Close>
       </ImageContainer>
     </ModalBackdrop>
-  )
+  );
 }
